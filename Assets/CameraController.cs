f2d05ab6ddbcb4417bb49046e662da90a535f2e4
lnq123 <nanqingl@uci.edu>
2017-05-03 07:21:25 -0700

    Add files via upload

---
+++ Assets/CameraController.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour {
+	public GameObject bullet;
+	private Vector3 offset;
+	// Use this for initialization
+	void Start () {
+		offset = transform.position;
+	}
+	
+	// Update is called once per frame
+	void LateUpdate () {
+
+		transform.position = bullet.transform.position + offset;
+	}
+//	public Transform target;
+//	public float smooth=5.0f;
+//	void Update(){
+//		transform.position = Vector3.Lerp (transform.position, target.position, Time.deltaTime * smooth);
+//	}
+}
---
+++ Assets/cameraTorocket.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class cameraTorocket : MonoBehaviour {
+
+//	var myPos:Vector3;
+//	var myPlay:Transform;
+//	function Update(){
+//		transform.postion = myPlay.position + myPos;
+//	}
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
